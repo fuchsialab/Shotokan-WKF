38c2822f44841ad3e1ca5634e286743d0bde41a9
juyelmajumder <justjuyel@gmail.com>
2022-07-10 12:48:09 +0530

    final

--- app/src/main/java/com/fuchsia/shotokanwkf/videoPlayer.java
+++ app/src/main/java/com/fuchsia/shotokanwkf/videoPlayer.java
@@ -10,6 +10,7 @@
  import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
  import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
  import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+ import com.pierfrancescosoffritti.androidyoutubeplayer.core.ui.PlayerUiController;
 
  import androidx.annotation.NonNull;
  import androidx.appcompat.app.AppCompatActivity;
